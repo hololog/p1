73bfb187a97a5161e32b0f591d536b6fcbdeed4b
hololog <93326521+hololog@users.noreply.github.com>
2022-01-30 06:57:24 +0900

    Create MemberDAO.java

---
+++ java/member/MemberDAO.java
@@ -0,0 +1,156 @@
+package member;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	//커넥션풀 접근제한자
+	public Connection getConnection() throws Exception{
+		Context init=new InitialContext();
+		DataSource ds=(DataSource) init.lookup("java:comp/env/jdbc/MysqlDB");
+		conn=ds.getConnection();
+		
+		return conn;
+	}
+	//DBclose
+	public void closeDB() {
+		
+		try {
+			if(conn!=null) conn.close();
+			if(pstmt!=null) pstmt.close();
+			if(rs!=null) rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	//DB 회원등록
+	public void insertMember(MemberDTO mDTO) {
+		String sql="INSERT INTO member VALUES(?,?,?,?,?,?,?)";
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, mDTO.getId());
+			pstmt.setString(2, mDTO.getPass());
+			pstmt.setString(3, mDTO.getName());
+			pstmt.setString(4, mDTO.getNick());
+			pstmt.setString(5, mDTO.getEmail());
+			pstmt.setString(6, mDTO.getAddress());
+			//DB에서 디폴트 뭐가 더 좋을까?
+			pstmt.setTimestamp(7, new Timestamp(System.currentTimeMillis()));
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeDB();
+		}
+	}//insertMember()
+	//로그인
+	public MemberDTO userCheck(MemberDTO loginDTO) {
+		String sql="SELECT * FROM member WHERE id=? and pass=?";
+		MemberDTO mDTO=null;
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, loginDTO.getId());
+			pstmt.setString(2, loginDTO.getPass());
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				mDTO=new MemberDTO();
+				
+				mDTO.setId(rs.getString("id"));
+				mDTO.setPass(rs.getString("pass"));
+				mDTO.setName(rs.getString("name"));
+				mDTO.setNick(rs.getString("nick"));
+				mDTO.setEmail(rs.getString("email"));
+				mDTO.setAddress(rs.getString("address"));
+				mDTO.setJoinDate(rs.getTimestamp("join_date"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeDB();
+		}
+		return mDTO;
+	}//userCheck()
+	//회원정보조회
+	public MemberDTO getUserInfo(String id) {
+		String sql="SELECT * FROM member WHERE id=?";
+		MemberDTO mDTO=null;
+		
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				mDTO=new MemberDTO();
+				mDTO.setId(rs.getString("id"));
+				mDTO.setPass(rs.getString("pass"));
+				mDTO.setName(rs.getString("name"));
+				mDTO.setNick(rs.getString("nick"));
+				mDTO.setEmail(rs.getString("email"));
+				mDTO.setAddress(rs.getString("address"));
+				mDTO.setJoinDate(rs.getTimestamp("join_date"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeDB();
+		}
+		return mDTO;
+	}
+	//회원정보 변경
+	public void updateInfo(MemberDTO mDTO) {
+		String sql="UPDATE member SET nick=?,name=?,email=?,address=? "
+				+ "WHERE id=?";
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, mDTO.getNick());
+			pstmt.setString(2, mDTO.getName());
+			pstmt.setString(3, mDTO.getEmail());
+			pstmt.setString(4, mDTO.getAddress());
+			pstmt.setString(5, mDTO.getId());
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeDB();
+		}
+		
+	}
+	//비밀번호 변경
+	public int updatePass(MemberDTO mDTO) {
+		String sql="UPDATE member SET pass=? WHERE id=?";
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, mDTO.getPass2());
+			pstmt.setString(2, mDTO.getId());
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1; //예외발생
+		} finally {
+			closeDB();
+		}
+		return 0;
+	}
+}
