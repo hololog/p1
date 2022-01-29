0b122ff08c5a560db4323e4b6aae8b9452361c0b
hololog <93326521+hololog@users.noreply.github.com>
2022-01-30 06:57:30 +0900

    Create MemberDTO.java

---
+++ java/member/MemberDTO.java
@@ -0,0 +1,67 @@
+package member;
+
+import java.sql.Timestamp;
+
+public class MemberDTO {
+	private String id;
+	private String nick;
+	private String pass;
+	private String pass2;
+	private String name;
+	private String email;
+	private String address;
+	private Timestamp joinDate;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getPass2() {
+		return pass2;
+	}
+	public void setPass2(String pass2) {
+		this.pass2 = pass2;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Timestamp getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Timestamp joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+	
+}
+
+
